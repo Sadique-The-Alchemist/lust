6678b7470b58cbb2c58dc9f9e2d3bcf4cc736726
Sadique-The-Alchemist <sadique.pal@gmail.com>
2024-12-28 00:56:19 +0530

    dictionary

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,8 +1,12 @@
 pub mod dictionary {
     use std::collections::hash_map::Entry;
     use std::collections::HashMap;
+
     pub const ERROR_NOT_FOUND: Result<&str, &str> =
         Err("could not found the word you were looking for");
+    pub const ERROR_WORD_EXIST: Result<String, &str> =
+        Err("cannot add word because it already exists");
+    pub const ERROR_WORD_NOT_EXIST: Result<String, &str> = Err("the word does not exist");
 
     pub fn search<'a>(
         dictionary: &'a HashMap<&'a str, &'a str>,
@@ -17,14 +21,43 @@ pub mod dictionary {
         dictionary: &'a mut HashMap<&'a str, &'a str>,
         key: &'a str,
         value: &'a str,
-    ) -> Result<(), &'static str> {
+    ) -> Result<String, &'static str> {
         match dictionary.entry(key) {
             Entry::Occupied(_entry) => {
-                return Err("cannot add word because it already exists");
+                return ERROR_WORD_EXIST;
             }
             Entry::Vacant(dictionary) => {
                 dictionary.insert(value);
-                Ok(())
+                Ok(format!("definition {} added for key {}", value, key))
+            }
+        }
+    }
+    pub fn update<'a>(
+        dictionary: &'a mut HashMap<&'a str, &'a str>,
+        key: &'a str,
+        value: &'a str,
+    ) -> Result<String, &'static str> {
+        match dictionary.entry(key) {
+            Entry::Occupied(_entry) => {
+                dictionary.insert(key, value);
+                let response = format!("updated {} to {}", key, value);
+                return Ok(response);
+            }
+
+            Entry::Vacant(_entry) => return ERROR_WORD_NOT_EXIST,
+        }
+    }
+    pub fn delete<'a>(
+        dictionary: &'a mut HashMap<&'a str, &'a str>,
+        key: &'a str,
+    ) -> Result<String, &'static str> {
+        match dictionary.entry(key) {
+            Entry::Occupied(_entry) => {
+                dictionary.remove(key);
+                return Ok(format!("the definition and key {} removed", key));
+            }
+            Entry::Vacant(_entry) => {
+                return ERROR_WORD_NOT_EXIST;
             }
         }
     }
--- tests/dictionary_test.rs
+++ tests/dictionary_test.rs
@@ -1,6 +1,12 @@
-use lust::dictionary::dictionary::{self, ERROR_NOT_FOUND};
+use lust::dictionary::dictionary::{self, ERROR_NOT_FOUND, ERROR_WORD_EXIST, ERROR_WORD_NOT_EXIST};
 use std::collections::HashMap;
 
+fn setup_dictionary() -> HashMap<&'static str, &'static str> {
+    return HashMap::from([("test", "this is just a test")]);
+}
+fn setup_dictionary_with_key(key: &str) -> HashMap<&str, &'static str> {
+    return HashMap::from([(key, "this is just a test")]);
+}
 #[test]
 fn search_known_word_test() {
     let mut dictionary = HashMap::new();
@@ -24,15 +30,61 @@ fn search_unknown_word_test() {
 #[test]
 fn test_add() {
     let mut dictionary = HashMap::new();
+    let word = "test";
+    let definition = "this is just a test";
 
-    let got = dictionary::add(&mut dictionary, "test", "this is just a test");
-    let want = Ok(());
+    let got = dictionary::add(&mut dictionary, word, definition);
+    let want = Ok(format!("definition {} added for key {}", definition, word));
     assert_eq!(got, want, "expected {:?} but got {:?}", want, got)
 }
 #[test]
 fn test_add_with_existing_key() {
-    let mut dictionary = HashMap::from([("test", "this is just a test")]);
+    let mut dictionary = setup_dictionary();
     let got = dictionary::add(&mut dictionary, "test", "this is just a test");
-    let want = Err("cannot add word because it already exists");
-    assert_eq!(got, want, "expected {:?} but got {:?} ", want, got)
+
+    assert_eq!(
+        got, ERROR_WORD_EXIST,
+        "expected {:?} but got {:?} ",
+        ERROR_WORD_EXIST, got
+    )
+}
+#[test]
+fn test_update() {
+    let word = "test";
+    let new_definition = "new definition";
+    let mut dictionary = setup_dictionary_with_key(&word);
+    let got = dictionary::update(&mut dictionary, word, new_definition);
+    let want = Ok(format!("updated {} to {}", word, new_definition));
+    assert_eq!(got, want, "expected{:?} but got {:?}", want, got)
+}
+#[test]
+fn test_update_non_existing_key() {
+    let mut dictionary = setup_dictionary();
+    let got = dictionary::update(&mut dictionary, "unknown", "unknown definition");
+
+    assert_eq!(
+        got, ERROR_WORD_NOT_EXIST,
+        "expectd {:?} but got {:?}",
+        ERROR_WORD_NOT_EXIST, got
+    )
+}
+
+#[test]
+fn test_delete() {
+    let key = "test";
+    let mut dictionary = setup_dictionary_with_key(key);
+    let got = dictionary::delete(&mut dictionary, key);
+    let want = Ok(format!("the definition and key {} removed", key));
+    assert_eq!(got, want, "expected {:?} but got {:?}", want, got);
+}
+
+#[test]
+fn test_delete_value_does_not_exist() {
+    let mut dictionary = setup_dictionary();
+    let got = dictionary::delete(&mut dictionary, "unknown");
+    assert_eq!(
+        got, ERROR_WORD_NOT_EXIST,
+        "expected {:?} but got {:?}",
+        ERROR_WORD_NOT_EXIST, got
+    )
 }
